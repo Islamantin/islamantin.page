85a5b8c2dc0d7c56b3fa4a23bab6dc731cfea4aa
Islamantin <islamantin@gmail.com>
2022-12-10 00:57:55 +0900

    changes test

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Islam Antin personal page!
         </h1>
 
         <p className={styles.description}>
