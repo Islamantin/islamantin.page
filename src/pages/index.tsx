da0551bbdb788cf9c2df603d2e76388945e5a5ad
Islamantin <islamantin@gmail.com>
2022-12-19 15:35:14 +0900

    color styles and font added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import imageUrlBuilder from "@sanity/image-url";
 import styles from "../styles/Home.module.scss";
 import sanityConfig from "../config/sanity.config";
 
+import colors from "../styles/colors.module.scss";
+import { useEffect } from "react";
+
 const client = sanityClient(sanityConfig);
 const imageBuilder = imageUrlBuilder(client);
 
@@ -15,6 +18,17 @@ function buildImage(source: any) {
 }
 
 export default function Home(data: any) {
+  useEffect(() => {
+    const colorsCount: number = +colors.colorsCount;
+    const colorInd = Math.floor(Math.random() * colorsCount + 1);
+    const body = document.getElementsByTagName("body")[0];
+    const classes: string[] = [];
+    for (let i = 1; i < colorsCount + 1; i++) {
+      classes.push("bgColor" + i);
+    }
+    body.classList.remove(...classes);
+    body.classList.add("bgColor" + colorInd);
+  });
   console.log(data);
   const posts = data.posts;
   return (
@@ -25,78 +39,45 @@ export default function Home(data: any) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Hi!</h1>
+        <div className={styles.introduction}>
+          <div className={styles.introText}>
+            <h1 className={styles.title}>Салям</h1>
 
-        <p className={styles.description}>
-          My name is <text className={styles.bold}>Islam Antin.</text>
-        </p>
+            <h2 className={styles.description}>
+              My name is <text>Islam</text>.
+              <br />
+              I'm Software Engineer and Designer.
+              <br /><br /> Welcome to my personal page!
+            </h2>
+          </div>
+          <Image className={styles.introImage} src="/me.jpg" width="300" height="300" alt="It's me"></Image>
+          {/* <img src="/profilepic.png" alt="it's me"></img> */}
+        </div>
 
         <div className={styles.grid}>
-          {posts.map((p: any, ind: number) => {
-            const width = 225;
-            const height = 400;
-            const image = buildImage(p.thumbnail).size(width, height).url();
-            return (
-              <a key={ind} href="#" className={styles.card}>
-                <h2>{p.title}</h2>
-                <img src={image}></img>
-                {/* <PortableText value={p.content}></PortableText> */}
-              </a>
-            );
-          })}
-
-          {/* <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a> 
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>*/}
+          {/* {posts &&
+            posts.map((p: any, ind: number) => {
+              const image = buildImage(p.thumbnail).url();
+              return (
+                <a key={ind} href="#" className={styles.card}>
+                  <h2>{p.title}</h2>
+                  <img src={image} className={styles.image}></img>
+                </a>
+                //<PortableText value={p.content}></PortableText>
+              );
+            })} */}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const result = await client.fetch(`*[_type == "post"]`);
-  console.log(result);
+  const result = await client.fetch(`{
+    "about": *[_id == "aboutMe"],
+    "posts": *[_type == "post"]
+  }`);
   return {
-    props: { posts: result },
+    props: result,
   };
 }
